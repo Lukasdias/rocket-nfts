6c0d289c2e355da41310211d101645d2b2440e59
Lukas Henrique Dias <diaslukas19@gmail.com>
2022-05-01 19:33:57 -0300

    [fix] - clean template and fix fade-in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,34 +9,37 @@ module.exports = {
     extend: {
       animation: {
         fadeIn: 'fadeIn 1s ease-in-out',
-        fadeOut: 'fadeOut 1s ease-in-out',
+        fadeOut: 'fadeOut 1s ease-in-out'
       },
       screens: {
-        "3xl": "1800px",
-        '4xl': "2020px",
+        '3xl': '1800px',
+        '4xl': '2020px'
       },
       keyframes: {
         fadeOut: {
           '0%': { opacity: 1 },
-          '100%': { opacity: 0 },
+          '100%': { opacity: 0 }
         },
         fadeIn: {
           '0%': { opacity: 0, transform: 'translateY(5vh)' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
-        },
+          '25%': { opacity: 0, transform: 'translateY(3.75vh)' },
+          '50%': { opacity: 0, transform: 'translateY(2.5vh)' },
+          '75%': { opacity: 0, transform: 'translateY(1.25vh)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' }
+        }
       },
       colors: {
-        'rocket-nft-background': "#0e0e0e",
-        'rocket-logo-color': "#ff5b50",
-        'rocket-card-color': "#252525",
-        'rocket-text-color': "#fff",
-        'rocket-subtitle-color': "#5a5757",
-        'rocket-input-color':  "#0D0D0D",
+        'rocket-nft-background': '#0e0e0e',
+        'rocket-logo-color': '#ff5b50',
+        'rocket-card-color': '#252525',
+        'rocket-text-color': '#fff',
+        'rocket-subtitle-color': '#5a5757',
+        'rocket-input-color': '#0D0D0D'
       }
     },
     fontFamily: {
       sans: ['Montserrat ', 'sans-serif'],
-      serif: ['Montserrat ', 'serif'],
+      serif: ['Montserrat ', 'serif']
     }
   },
   plugins: []
